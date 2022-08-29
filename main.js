ef62ee3548763f2f55ae4cfa325e77e6b779195e
Julián Astudillo <julian12astudillo@gmail.com>
2022-08-29 15:06:31 -0300

    MCGA - 02
    Added node, express and nodemon pacages.

---
+++ main.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const product = require('./data/products.json')
+const app = express()
+const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.get('/', (req, res) => {
+  res.send(wantedData)
+})
+
+app.get('/products', (req, res) => {
+    res.json(product)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+let  wantedData = product.filter(function(i) {
+    return i.id === 10;
+  });
