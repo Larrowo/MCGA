fb2c51427dd2ca0431e9d9172d4d281f5d847858
Julián Astudillo <julian12astudillo@gmail.com>
2022-08-30 16:14:41 -0300

    MCGA - 03
    Added new routes.

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
-const express = require('express')
-const product = require('./data/products.json')
-const app = express()
-const port = 3000
+const express = require("express");
+const products = require("./data/products.json");
+const employees = require("./data/employees.json");
+const companies = require("./data/companies.json");
+const app = express();
+const port = 3000;
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
 
-app.get('/', (req, res) => {
-  res.send(wantedData)
-})
+app.get("/", (req, res) => {
+  res.send(wantedData);
+});
 
-app.get('/products', (req, res) => {
-    res.json(product)
-})
+app.get("/login", (req, res) => {
+  res.send("login");
+});
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/employees", (req, res) => {
+  res.json(employees);
+});
+
+app.get("/companies", (req, res) => {
+  res.json(companies);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-let  wantedData = product.filter(function(i) {
-    return i.id === 10;
-  });
+let wantedData = products.filter(function (i) {
+  return i.id === 10;
+});
